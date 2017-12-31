b6588bb8ef508444bcec62054cd514f5de7f3ba0
daranda99 <34823449+daranda99@users.noreply.github.com>
2017-12-30 16:37:32 -0800

    Create authorAge.rb

---
+++ LICENSE.md/authorAge.rb
@@ -0,0 +1,5 @@
+irb(main):029:0> # days a year * hours a day * minutes a hour * seconds a minute * amount of years + (days a year * hours a day * minutesa hour * seconds a minute * amount of leap years)
+irb(main):030:0* puts 365 * 24 * 60 * 60 * 30 + (366 * 24 * 60 * 60 * 10)
+1262304000
+irb(main):034:0> puts 1262304000/(60*60*24*365.25)
+40.0
